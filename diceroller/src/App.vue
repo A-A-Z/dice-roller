4c1e8fd93d176352664d4704365986b218d43b86
A-A-Z <dramatichamster@gmail.com>
2017-03-05 19:46:40 +1100

    Got the basics working.  Those dice are rolling.

--- diceroller/src/App.vue
+++ diceroller/src/App.vue
@@ -1,28 +1,60 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
-  </div>
+  <main id="app">
+    <header>
+      <h1>test</h1>
+    </header>
+    <section>
+      <input type="number" min="1" max="6" v-model="number" @change="buildDice">
+      <button v-on:click="roll">Roll</button>
+    </section>
+    <section>
+      <ul>
+        <li v-for="die in dice">
+          {{ die }}
+        </li>
+      </ul>
+    </section>
+  </main>
 </template>
 
 <script>
-import Hello from './components/Hello'
+
+let data = {
+  number: 3,
+  dice: []
+}
 
 export default {
   name: 'app',
-  components: {
-    Hello
+  data: () => {
+    return data
+  },
+  methods: {
+    buildDice: () => {
+      let dice = []
+
+      for (let i = 0; i < data.number; i++) {
+        dice.push(1)
+      }
+
+      data.dice = dice
+    },
+    roll: (event) => {
+      let dice = data.dice.slice()
+
+      for (let i = 0; i < data.number; i++) {
+        dice[i] = Math.floor((Math.random() * 6) + 1)
+      }
+
+      data.dice = dice
+    }
+  },
+  mounted () {
+    this.buildDice()
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+   @import './assets/sass/app.scss'
 </style>
